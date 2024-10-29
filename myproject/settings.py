ce9e68e3ab59ccb1013ab464f5cc4decc8c406c6
aswathymj <aswathymj2025@mca.ajce.in>
2024-10-29 15:36:35 +0530

    fourth commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,8 +30,9 @@ AUTH_USER_MODEL = 'myapp.CustomUser'
 # settings.py
 
 MIDDLEWARE = [
-     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
